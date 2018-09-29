0105fc8e1aa32ec73d3cff0d055337d25f3b8ea8
tarvo <tarvo@sci.fi>
2018-09-29 15:58:17 +0300

    Tehtävä 1.3 tehty

--- teht1.1/src/index.js
+++ teht1.1/src/index.js
@@ -35,20 +35,26 @@ const Otsikko = (props) => {
     )
   }
 
-const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
-
+  const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    }
+    const osa2 = {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    }
+    const osa3 = {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3}/>
-      <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3}/>
+      <Sisalto osa1={osa1.nimi} tehtavia1={osa1.tehtavia} osa2={osa2.nimi} tehtavia2={osa2.tehtavia} osa3={osa3.nimi} tehtavia3={osa3.tehtavia}/>
+      <Yhteensa tehtavia={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia}/>
     </div>
   )
 }
